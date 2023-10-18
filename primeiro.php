7bebe15ec58beda3cff549d34ac668125884ea4e
Felipe Giglio <felipe.gigliosilva@gmail.com>
2023-10-18 16:55:55 -0300

    novo

--- primeiro.php
+++ primeiro.php
@@ -1,6 +1,16 @@
 <?php
-    $var = array('felipe','lucelia');
+    $var = array('felipe', 'lucelia');
     echo $var[0];
+    echo '<br>';
 
-    foreach()  
-?>
+// //i did not understend
+// foreach($var as $key => $value){
+//     echo $key;
+//     echo '=>';
+// }  
+
+$total = count($var);
+for ($i = 0; $i < $total; $i++) {
+    echo $var[$i];
+    echo '<br>';
+}
